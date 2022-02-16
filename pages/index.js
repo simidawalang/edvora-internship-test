6be5c01b1bf0a1be8c3e1998fb4b30b8b42aa635
simidawalang <simidawalang@gmail.com>
2022-02-16 23:26:57 +0100

    implemented filter

--- pages/index.js
+++ pages/index.js
@@ -27,18 +27,47 @@ export default function Home() {
   );
 
   const [result, setResult] = useState(sortedRides);
+  const upcomingRides = rides.filter((r) => r.date * 1000 > Date.now());
+  const pastRides = rides.filter((r) => r.date * 1000 < Date.now());
+
   const [showFilter, setShowFilter] = useState(false);
-  const [state, setState] = useState("");
-  const [city, setCity] = useState("");
+  const cities = [...new Set(rides.map(({ city }) => city))];
+  const states = [...new Set(rides.map(({ state }) => state))];
+  // To have unique values in case there are duplicate cities or states
+
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
 
   const filterRef = useRef();
 
-  const filterRides = (e) => {
-    const { value } = e.target;
-  };
-
   const openFilter = () => {
     setShowFilter(true);
+  };
+
+  const handleStateChange = (e) => {
+    const { value } = e.target;
+    setSelectedState(value);
+    value.trim().length === 0 && setResult(sortedRides);
+    value.trim().length !== 0 &&
+      setResult(
+        sortedRides.filter((r) => r.state.toLowerCase() === value.toLowerCase())
+      );
+    setSelectedCity('');
+  };
+
+
+  const handleCityChange = (e) => {
+    const { value } = e.target;
+    setSelectedCity(value);
+    value.trim().length === 0 && setSelectedState(selectedState);
+    value.trim().length !== 0 &&
+      setResult(
+        sortedRides.filter(
+          (r) =>
+            r.city.toLowerCase() === value.toLowerCase() &&
+            r.state.toLowerCase() === selectedState.toLowerCase()
+        )
+      );
   };
 
   useEffect(() => {
@@ -68,9 +97,15 @@ export default function Home() {
       </Head>
       <nav>
         <ul>
-          <li>Nearest Rides (4)</li>
-          <li>Upcoming Rides (2)</li>
-          <li>Past Rides (3)</li>
+          <li onClick={() => setResult(rides)}>
+            Nearest Rides ({rides.length})
+          </li>
+          <li onClick={() => setResult(upcomingRides)}>
+            Upcoming Rides ({upcomingRides.length})
+          </li>
+          <li onClick={() => setResult(pastRides)}>
+            Past Rides ({pastRides.length})
+          </li>
         </ul>
         <span className="filter-container" onClick={openFilter}>
           <FilterIcon />
@@ -80,34 +115,54 @@ export default function Home() {
           <Filter onClick={openFilter} ref={filterRef}>
             <h3>Filter</h3>
             <div className="options">
-              <select id="">
+              <select id="" value={selectedState} onChange={handleStateChange}>
                 <option value="" defaultValue>
                   State
                 </option>
+                <option value="some state">Some State</option>
+                {states.map((state) => (
+                  <option id={state} value={state} key={state}>
+                    {state}
+                  </option>
+                ))}
               </select>
-              <select name="" id="">
+              <select
+                name=""
+                id=""
+                value={selectedCity}
+                onChange={handleCityChange}
+                disabled={selectedState.trim().length === 0}
+              >
                 <option value="" defaultChecked>
                   City
                 </option>
+                {cities.map((city) => (
+                  <option id={city} value={city} key={city}>
+                    {city}
+                  </option>
+                ))}
+                <option value="some city">Some city</option>
               </select>
             </div>
           </Filter>
         )}
       </nav>
       <div className="ride-container">
-        {result.map(({ id, origin_station_code, station_path, date }) => {
-          let distance = calculateDistance(station_path);
-          return (
-            <Ride
-              key={id}
-              id={id}
-              originStationCode={origin_station_code}
-              stationPath={station_path}
-              date={date}
-              distance={distance}
-            />
-          );
-        })}
+        {result.length > 0
+          ? result.map(({ id, origin_station_code, station_path, date }) => {
+              let distance = calculateDistance(station_path);
+              return (
+                <Ride
+                  key={id}
+                  id={id}
+                  originStationCode={origin_station_code}
+                  stationPath={station_path}
+                  date={date}
+                  distance={distance}
+                />
+              );
+            })
+          : "No result found"}
       </div>
     </div>
   );
